42b0b442074f414076b2c23983b04148cc5592d2
Pol <polnadalserra@gmail.com>
2020-02-04 19:56:02 +0100

    Initial commit

---
+++ app/src/main/java/com/stucom/thearchive/EstanteriaActivity.java
@@ -0,0 +1,14 @@
+package com.stucom.thearchive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstanteriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estanteria);
+    }
+}
