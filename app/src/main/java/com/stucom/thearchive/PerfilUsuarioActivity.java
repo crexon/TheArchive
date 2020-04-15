e2c3d957e4f55418e74d49f1d9b8773329ebf29d
josepprofitos <josepprofitos99@gmail.com>
2020-04-15 15:21:26 +0200

    Commit del perfil de usuario

---
+++ app/src/main/java/com/stucom/thearchive/PerfilUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.stucom.thearchive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
