be5a9f43734a61c7067c558745138e66a3109317
josepprofitos <josepprofitos99@gmail.com>
2020-04-16 19:18:26 +0200

    Commit del perfil de usuario y el editar perfil

--- app/src/main/java/com/stucom/thearchive/PerfilUsuarioActivity.java
+++ app/src/main/java/com/stucom/thearchive/PerfilUsuarioActivity.java
@@ -1,14 +1,57 @@
 package com.stucom.thearchive;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilUsuarioActivity extends AppCompatActivity {
+
+    Button btnEditProfile, btnSeeBooks, btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
+        btnEditProfile = findViewById(R.id.btnEditProfile);
+        btnSeeBooks = findViewById(R.id.btnSeeBooks);
+        btnSettings = findViewById(R.id.btnSettings);
+
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToEditarPerfil();
+            }
+        });
+
+        btnSeeBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToEstanteriaActivity();
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    private void moveToEditarPerfil() {
+        Intent intent = new Intent(PerfilUsuarioActivity.this, EditarPerfilActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void moveToEstanteriaActivity() {
+        Intent intent = new Intent(PerfilUsuarioActivity.this, EstanteriaActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
