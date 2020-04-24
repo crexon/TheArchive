14fce564cf4b53fa5dfdd5a050cbf275ea9b73bc
regormar <regormar99@gmail.com>
2020-04-24 16:36:58 +0200

    Creación vista de estantería

--- app/src/main/java/com/stucom/thearchive/EstanteriaActivity.java
+++ app/src/main/java/com/stucom/thearchive/EstanteriaActivity.java
@@ -2,13 +2,45 @@ package com.stucom.thearchive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class EstanteriaActivity extends AppCompatActivity {
+public class EstanteriaActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int[] BUTTONS_ID = new int[] {
+            R.id.btnSeeAll, R.id.btnRead, R.id.btnReading, R.id.btnWantTo
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estanteria);
+
+        for(int id: BUTTONS_ID) {
+            Button button = findViewById(id);
+            button.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(EstanteriaActivity.this, ShelvesActivity.class);;
+        switch(view.getId()) {
+            case R.id.btnSeeAll:
+                intent.putExtra("type", "all");
+                break;
+            case R.id.btnRead:
+                intent.putExtra("type", "read");
+                break;
+            case R.id.btnReading:
+                intent.putExtra("type", "reading");
+                break;
+            case R.id.btnWantTo:
+                intent.putExtra("type", "want");
+                break;
+        }
+        if (intent == null) return;
+        startActivity(intent);
     }
 }
