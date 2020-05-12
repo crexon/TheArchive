c600f0120a900ed7e9b99ea20a5556b68ca9618c
pol <polnadalserra@gmail.com>
2020-05-12 23:27:36 +0200

    - Implementación de la feed totalmente conectado con el backend. Se registra las acciones de agregar libro y modificación de perfil.

--- app/src/main/java/com/stucom/thearchive/modelo_navigation/FeedFragment.java
+++ app/src/main/java/com/stucom/thearchive/modelo_navigation/FeedFragment.java
@@ -6,12 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.muddzdev.styleabletoast.StyleableToast;
 import com.stucom.thearchive.R;
 import com.stucom.thearchive.modelo_feed.Feed;
 
@@ -27,26 +38,41 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_feed, container, false);
 
+        // Type 1 = leido, 2 = leyendo, 3 = quiere leer (no), 4 = perfila actualizado
         rcFeeds = v.findViewById(R.id.rcFeed);
         rcFeeds.setLayoutManager(new LinearLayoutManager(getContext()));
-        initData();
+        downloadData();
 
         return v;
 
     }
 
-    private void initData(){
-        Feed feed = new Feed("crexon", 1, "2020-05-12");
-        Feed feed2 = new Feed("oripik", 3, "2020-03-03");
-        Feed feed3 = new Feed("test", 2, "2020-04-06");
+    private void downloadData(){
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = "http://169.254.25.54:8000/archive/feed/";
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-        List<Feed> feeds = new ArrayList<>();
-        feeds.add(feed);
-        feeds.add(feed2);
-        feeds.add(feed3);
+                        Gson gson = new Gson();
+                        List<Feed> feeds = gson.fromJson(response, new TypeToken<List<Feed>>(){}.getType());
 
-        FeedAdapter adapter = new FeedAdapter(feeds);
-        rcFeeds.setAdapter(adapter);
+                        FeedAdapter adapter = new FeedAdapter(feeds);
+                        rcFeeds.setAdapter(adapter);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        StyleableToast.makeText(getActivity(), getActivity().getString(R.string.content_download), Toast.LENGTH_LONG, R.style.toast).show();
+                    }
+                }
+        );
+        queue.add(request);
     }
 
 
@@ -54,7 +80,7 @@ public class FeedFragment extends Fragment {
 
         class ViewHolder extends RecyclerView.ViewHolder {
             ImageView ivPhoto;
-            TextView tvFeedName, tvFeedDate;
+            TextView tvFeedName, tvFeedDate, tvFeedDesc;
 
 
             ViewHolder(View view) {
@@ -62,6 +88,7 @@ public class FeedFragment extends Fragment {
                 ivPhoto = view.findViewById(R.id.photo);
                 tvFeedName = view.findViewById(R.id.tvFeedName);
                 tvFeedDate = view.findViewById(R.id.tvFeedDate);
+                tvFeedDesc = view.findViewById(R.id.tvFeedDesc);
             }
         }
 
@@ -85,6 +112,13 @@ public class FeedFragment extends Fragment {
         public void onBindViewHolder(@NonNull FeedFragment.FeedAdapter.ViewHolder holder, int position) {
             Feed feed = feeds.get(position);
             holder.tvFeedName.setText(feed.getUsername());
+
+            switch (feed.getType()){
+                case 1: holder.tvFeedDesc.setText(feed.getUsername() + " ha agregado un libro a su estanteria como leído."); break;
+                case 2: holder.tvFeedDesc.setText(feed.getUsername() + " ha agregado un libro a su estanteria  que esta leyendo actualmente."); break;
+                case 3: holder.tvFeedDesc.setText(feed.getUsername() + " ha agregado un libro que le gustaría leer."); break;
+                case 4: holder.tvFeedDesc.setText(feed.getUsername() + " ha actualizado su perfil."); break;
+            }
             holder.tvFeedDate.setText(feed.getDate());
         }
 
@@ -94,4 +128,9 @@ public class FeedFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        downloadData();
+    }
 }
